a23fd0bc2da3e2e9b96545b33a3b2329ff4df4e8
elyons <elyons@engin.umass.edu>
2021-03-13 17:53:13 +0000

    swapped out returnp color scale to match legend

--- geojson_conversions/returnp_to_geojson.py
+++ geojson_conversions/returnp_to_geojson.py
@@ -61,35 +61,53 @@ class returnp_to_geojson(object):
                         pol = gj.Polygon([[(-1.*ny[ii,jj],nx[ii,jj]),(-1.*ny[ii,jj+1],nx[ii,jj+1]),(-1.*ny[ii-1,jj+1],nx[ii-1,jj+1]),(-1.*ny[ii-1,jj],nx[ii-1,jj]),(-1.*ny[ii,jj], nx[ii,jj])]])
                         
                         if (returnp[ii,jj]>1 and returnp[ii,jj]<=2):
-                            polcolor = 'cyan'
+                            polcolor = '#4cecec'
                         elif (returnp[ii,jj]>2 and returnp[ii,jj]<=3):
-                            polcolor = 'royalblue'
+                            polcolor = '#44c6f0'
                         elif (returnp[ii,jj]>3 and returnp[ii,jj]<=4):
-                            polcolor = 'darkblue'
+                            polcolor = '#429afb'
                         elif (returnp[ii,jj]>4 and returnp[ii,jj]<=5):
-                            polcolor = 'lawngreen'
+                            polcolor = '#3431fd'
                         elif (returnp[ii,jj]>5 and returnp[ii,jj]<=6):
-                            polcolor = 'forestgreen'       
+                            polcolor = '#40f600'       
                         elif (returnp[ii,jj]>6 and returnp[ii,jj]<=7):
-                            polcolor = 'darkgreen' 
+                            polcolor = '#3ada0b' 
                         elif (returnp[ii,jj]>7 and returnp[ii,jj]<=8):
-                            polcolor = 'yellow'    
-                        elif (returnp[ii,jj]>8 and returnp[ii,jj]<=9):
-                            polcolor = 'orange'   
-                        elif (returnp[ii,jj]>9 and returnp[ii,jj]<=10):
-                            polcolor = 'darkorange'
-                        elif (returnp[ii,jj]>10 and returnp[ii,jj]<=11):
-                            polcolor = 'orangered'  
-                        elif (returnp[ii,jj]>11 and returnp[ii,jj]<=12):
-                            polcolor = 'red' 
-                        elif (returnp[ii,jj]>12 and returnp[ii,jj]<=13):
-                            polcolor = 'darkred'    
-                        elif (returnp[ii,jj]>13 and returnp[ii,jj]<=14):
-                            polcolor = 'mediumvioletred'     
-                        elif (returnp[ii,jj]>14 and returnp[ii,jj]<=15):
-                            polcolor = 'purple'     
-                        elif (returnp[ii,jj]>15):
-                            polcolor = 'white'
+                            polcolor = '#2eb612'    
+                        elif (returnp[ii,jj]>8 and returnp[ii,jj]<=10):
+                            polcolor = '#2a8a0f'   
+                        elif (returnp[ii,jj]>10 and returnp[ii,jj]<=12):
+                            polcolor = '#f8f915'
+                        elif (returnp[ii,jj]>12 and returnp[ii,jj]<=14):
+                            polcolor = '#e9d11c'  
+                        elif (returnp[ii,jj]>14 and returnp[ii,jj]<=16):
+                            polcolor = '#dcb11f' 
+                        elif (returnp[ii,jj]>16 and returnp[ii,jj]<=18):
+                            polcolor = '#bd751f'    
+                        elif (returnp[ii,jj]>18 and returnp[ii,jj]<=20):
+                            polcolor = '#f39a9c'     
+                        elif (returnp[ii,jj]>20 and returnp[ii,jj]<=25):
+                            polcolor = '#f23a43'
+                        elif (returnp[ii,jj]>25 and returnp[ii,jj]<=30):
+                            polcolor = '#da1622'
+                        elif (returnp[ii,jj]>30 and returnp[ii,jj]<=35):
+                            polcolor = '#a90c1b'
+                        elif (returnp[ii,jj]>35 and returnp[ii,jj]<=40):
+                            polcolor = '#fa31ff'
+                        elif (returnp[ii,jj]>40 and returnp[ii,jj]<=50):
+                            polcolor = '#d32ada'
+                        elif (returnp[ii,jj]>50 and returnp[ii,jj]<=60):
+                            polcolor = '#9f1fa3'
+                        elif (returnp[ii,jj]>60 and returnp[ii,jj]<=75):
+                            polcolor = '#751678'
+                        elif (returnp[ii,jj]>75 and returnp[ii,jj]<=100):
+                            polcolor = '#ffffff'
+                        elif (returnp[ii,jj]>100 and returnp[ii,jj]<=200):
+                            polcolor = '#c1bdff'
+                        elif (returnp[ii,jj]>200 and returnp[ii,jj]<=500):
+                            polcolor = '#c5ffff'
+                        elif (returnp[ii,jj]>500):
+                            polcolor = '#fcfec0'
                             
                         feature = gj.Feature(geometry=pol, properties={"color": polcolor, "dataType": "hydrology", "productType": "return period", "value": returnp[ii,jj]}, "units": "years")
                         polygon_feats.append(feature)
