b8235e36baf4caa1bb1fd1919b3ec941cdc3faa1
elyons <elyons@engin.umass.edu>
2021-03-15 17:22:59 +0000

    updated send_latest_output.sh

--- send_latest_output.sh
+++ send_latest_output.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cd /nfs/shared/rdhm; tar -cf /nfs/shared/ldm/rdhm_output.tar `for l in \`ls -d *\`; do ls output/$l/*z.gz | tail -1; done`  `for l in \`ls -d *\`; do ls output/$l/current* ; done`; /home/ldm/bin/pqinsert -f EXP -p rdhm_output.tar /nfs/shared/ldm/rdhm_output.tar; rm /nfs/shared/ldm/rdhm_output.tar;
+pushd /nfs/shared/rdhm; tar -cf /nfs/shared/ldm/rdhm_output.tar `for l in \`ls -d output/*\`; do ls $l/*z.gz | tail -1; done`  `for l in \`ls -d output/*\`; do ls $l/current* ; done`;  popd
+/home/ldm/bin/pqinsert -f EXP -p rdhm_output.tar /nfs/shared/ldm/rdhm_output.tar; 
+rm /nfs/shared/ldm/rdhm_output.tar; popd
