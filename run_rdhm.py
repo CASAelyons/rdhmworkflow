825e04ae855a317f7b95fd51b800370e0594f714
elyons <elyons@engin.umass.edu>
2021-03-15 00:35:24 +0000

    made some changes to reduce hard coded paths also some logging lines to debug path call not working in background those may eventually be removed

--- run_rdhm.py
+++ run_rdhm.py
@@ -20,10 +20,16 @@ class rdhmWorkflow(object):
         self.starttime = starttime
         self.endtime = endtime
     def generate_jobs(self):
-        
+        logging.critical('In workflow\n')
         ts = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
         wf = Workflow("casa_rdhm_wf-%s" % ts)
-
+        wfpath = str(Path.home()) + '/rdhmworkflow/'
+        logging.critical('workflow path: ' + wfpath + '\n')
+        
+        local_storage_path = wfpath + 'output'
+        local_scratch_path = wfpath + 'scratch'
+        f_local_storage_path = "file://" + local_storage_path
+        f_local_scratch_path = "file://" + local_scratch_path
         sc = SiteCatalog()
         
         shared_scratch = Directory(Directory.SHARED_SCRATCH, path="/nfs/shared/pegasus/scratch")\
@@ -32,14 +38,17 @@ class rdhmWorkflow(object):
         #container_location = Directory(Directory.SHARED_STORAGE, path="/nfs/shared/ldm")\
         #        .add_file_servers(FileServer("file:///nfs/shared/ldm", Operation.ALL))
 
-        local_storage = Directory(Directory.LOCAL_STORAGE, "/home/ldm/rdhmworkflow/output")\
-                .add_file_servers(FileServer("file:///home/ldm/rdhmworkflow/output", Operation.ALL))
-        
+        local_storage = Directory(Directory.LOCAL_STORAGE, path=local_storage_path)\
+                .add_file_servers(FileServer(f_local_storage_path, Operation.ALL))
+
+        local_scratch = Directory(Directory.LOCAL_SCRATCH, path=local_scratch_path)\
+                .add_file_servers(FileServer(f_local_scratch_path, Operation.ALL))
+
         #local = Site("local", arch=Arch.X86_64, os_type=OS.LINUX, os_release="rhel", os_version="7")
         local = Site("local")
 
         #local.add_directories(shared_scratch,local_storage, container_location)
-        local.add_directories(shared_scratch,local_storage)
+        local.add_directories(shared_scratch,local_storage,local_scratch)
 
         #exec_site = Site("condorpool", arch=Arch.X86_64, os_type=OS.LINUX, os_release="rhel", os_version="7")
         exec_site = Site("condorpool")
@@ -87,25 +96,33 @@ class rdhmWorkflow(object):
         props = Properties()
         props["pegasus.transfer.links"]="true"
         props["pegasus.transfer.bypass.input.staging"]="true"
-        props.write()
+        #logging.critical('writing properties\n')
+        propfilepath = wfpath + 'pegasus.properties'
+        logging.critical('writing properties to:' + propfilepath + '\n')
+        with open(propfilepath, "w") as f:
+            props.write(f)
         
+        logging.critical('creating job')
         rdhm_job = Job(rdhm_transformation)\
             .add_args("-s", self.starttime, "-f", self.endtime, inputfile)\
             .add_inputs(inputfile)
-
+        logging.critical('adding jobs and catalogs\n')
         wf.add_jobs(rdhm_job)
-
+        logging.critical('addjob\n')
         wf.add_site_catalog(sc)
         wf.add_replica_catalog(rc)
         wf.add_transformation_catalog(tc)
         
+        logging.critical('trying to plan\n')
         try:
-            wf.plan(submit=True)
+            wf.plan(conf=propfilepath, dir=wfpath, verbose=3, submit=True)
+            #wf.plan(conf=propfilepath, dir=wfpath, verbose=3)
+        except PegasusClientError as e:
+            print(e.output)
+        logging.critical('planned\n')
             #wf.wait()
             #wf.analyze()
             #wf.statistics()
-        except PegasusClientError as e:
-            print(e.output)
         
     
     def generate_workflow(self):
@@ -113,8 +130,8 @@ class rdhmWorkflow(object):
         self.generate_jobs()
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-
+    logging.basicConfig(level=logging.DEBUG, filename="/home/ldm/rdhmworkflow/perl/pegasusPy.log")
+    logging.critical('Initiated!')
     parser = ArgumentParser(description="RDHM Workflow")
     parser.add_argument("-i", "--inputfile", metavar="INPUT_FILE", type=str, help="Path to input card (configfile)", required=True)
     parser.add_argument("-s", "--starttime", metavar="START_TIME", type=str, help="Start time in ISO format-> YYYYMMDDTHHMM (use UTC)", required=True)
@@ -123,6 +140,7 @@ if __name__ == '__main__':
     inputfile = args.inputfile
     starttime = args.starttime
     endtime = args.endtime
+    logging.critical('Calling workflow')
     workflow = rdhmWorkflow(inputfile, starttime, endtime)
     workflow.generate_workflow()
 
